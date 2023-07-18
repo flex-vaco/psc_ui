ca50768c9cea1fa614ed6ae38196085b24ebe5cd
Dinesh Kumar Subramanian <dinesh.subramanian@getbuilt.com>
2023-07-18 19:14:00 +0530

    fancy Date Picker for timesheet filter

--- src/components/TimesheetFilters.tsx
+++ src/components/TimesheetFilters.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "./TaskEffort.css";
 import { TimesheetEntries } from "../types/TimesheetEntries";
 import { User } from "../types/User";
@@ -8,6 +8,11 @@ import { HoursType } from "../pages/timesheet/FancyTimesheet";
 import "./TaskEffort.css";
 import Dropdown, { Option } from "react-dropdown";
 import "react-dropdown/style.css";
+import {
+  OnDatesChangeProps,
+} from "@datepicker-react/styled";
+import { DateRangeInput } from "@datepicker-react/styled";
+import { ThemeProvider } from "styled-components";
 
 export interface FiltersProps {
   userInfo: User;
@@ -23,12 +28,32 @@ export interface FiltersProps {
 }
 
 const TimesheetFilters = (props: FiltersProps) => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const initialState = {
+    startDate: new Date(props.startDate),
+    endDate: new Date(props.endDate),
+    focusedInput: null,
+  };
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    props.handleDateFilter(startDate, endDate);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function reducer(state: any, action: any) {
+    switch (action.type) {
+      case "focusChange":
+        return { ...state, focusedInput: action.payload };
+      case "dateChange":
+        return action.payload;
+      default:
+        throw new Error();
+    }
+  }
+
+  const handleDateSelection = (data: OnDatesChangeProps) => {
+    if (data?.startDate != null && data?.endDate != null) {
+      props.handleDateFilter(
+        data.startDate?.toString() ?? "",
+        data.endDate?.toString() ?? ""
+      );
+    }
   };
 
   const options = props.empProjects.map((empProjects) => {
@@ -52,57 +77,41 @@ const TimesheetFilters = (props: FiltersProps) => {
           <div className="email">{`Role: ${props.userInfo.role}`}</div>
         </div>
         <div className="col-3"></div>
-        <div className="col-3">
-          
-        </div>
+        <div className="col-3"></div>
       </div>
       <div className="row">
         <div className="col-7">
-          <form onSubmit={handleSubmit}>
-            <div className="row">
-              <div className="col-3">
-                <div className="form-group float-start mb-2 me-2">
-                  <label htmlFor="dispStartDate">Start Date</label>
-                  <input
-                    className="form-control"
-                    type="date"
-                    name="startDate"
-                    id="startDate"
-                    defaultValue={props.startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="col-3">
-                <div className="form-group float-start">
-                  <label htmlFor="dispEndDate">End Date</label>
-                  <input
-                    className="form-control"
-                    type="date"
-                    name="endDate"
-                    id="endDate"
-                    defaultValue={props.endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="col-3">
-                <div className="form-group float-start mb-2 me-2">
-                  <label htmlFor="get"> </label>
-                  <button
-                    onClick={(event) => {
-                      handleSubmit(event);
-                    }}
-                    type="submit"
-                    name="get"
-                    className="form-control btn btn-outline-info me-2"
-                  >
-                    Filter
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
+            <ThemeProvider
+            theme={{
+              breakpoints: ["32em", "48em", "64em"],
+              reactDatepicker: {
+                daySize: [30, 34],
+                fontFamily: "system-ui, -apple-system",
+                colors: {
+                  accessibility: "#D80249",
+                  selectedDay: "#f7518b",
+                  selectedDayHover: "#F75D95",
+                  primaryColor: "#d8366f",
+                },
+                dateRangeZIndex: "999",
+              },
+            }}
+            
+          >
+            <DateRangeInput
+              onDatesChange={(data) => {
+                dispatch({ type: "dateChange", payload: data });
+                handleDateSelection(data);
+              }}
+              onFocusChange={(focusedInput) =>
+                dispatch({ type: "focusChange", payload: focusedInput })
+              }
+              startDate={state.startDate} // Date or null
+              endDate={state.endDate} // Date or null
+              focusedInput={state.focusedInput} // START_DATE, END_DATE or null
+            />
+          </ThemeProvider>
+
         </div>
         <div className="col-3"></div>
         <div className="col-2">
@@ -113,12 +122,11 @@ const TimesheetFilters = (props: FiltersProps) => {
                 (a) => a?.projectDetails?.projectId === props.selectedProjectId
               )[0]?.projectDetails?.projectName
             }
-            onChange={(args) =>
-              props.handleProjectFilter(Number(args?.value))
-            }
+            onChange={(args) => props.handleProjectFilter(Number(args?.value))}
           />
         </div>
       </div>
+      <div className="row"><br></br></div>
       <div className="row">
         <HoursDashboard
           startDate={props.startDate}
