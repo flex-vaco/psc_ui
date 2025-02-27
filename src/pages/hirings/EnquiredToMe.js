fe0787fb84550a44a55369139f1a7c0107310b6b
narasimhavaco <narasimha.daruru@vacobinary.in>
2025-02-27 14:32:52 +0530

    Hiring Fixes

--- src/pages/hirings/EnquiredToMe.js
+++ src/pages/hirings/EnquiredToMe.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import Layout from "../../components/Layout"
 import * as Utils from "../../lib/Utils";
 import HiringModal from '../../components/HiringModal';
+import DataTable from 'react-data-table-component';
 
 function EnquiredByMe() {
     const [hiringList, setHiringList] = useState([])
@@ -27,24 +28,56 @@ function EnquiredByMe() {
         })
     }
 
-    const openModal = (hiringId) => {
-        setIsOpen(true);
-        axios.get(`/hirings/${hiringId}`)
+    const columns = [
+      { name: 'Employee Name', selector: row => `${row.employee_details.first_name} ${row.employee_details.last_name}`, sortable: false },
+      { name: 'Enquired Manager Name', selector: row => `${row.manager_details.manager_first_name} ${row.manager_details.manager_last_name}`, sortable: false },
+      { name: 'Enquired Manager Email', selector: row => row.manager_details.manager_email, sortable: false },
+      { name: 'Project Name', selector: row => row.project_name, sortable: false },
+      { name: 'Start Date', selector: row => new Date(row.start_date).toLocaleDateString(), sortable: false },
+      { name: 'End Date', selector: row => new Date(row.end_date).toLocaleDateString(), sortable: false },
+      { name: 'Work Location', selector: row => row.work_location, sortable: false },
+      { name: 'Hiring Status', selector: row => row.hiring_status, sortable: false }
+    ];
+
+    const [selectedStatus, setSelectedStatus] = useState('');
+    const [searchText, setSearchText] = useState('');
+
+    const filteredData = hiringList.filter(item => {
+      const isStatusMatch = selectedStatus ? item.hiring_status === selectedStatus : true;
+      const isSearchMatch = searchText ? (
+        `${item.employee_details.first_name} ${item.employee_details.last_name}`.toLowerCase().includes(searchText.toLowerCase()) ||
+        `${item.manager_details.manager_first_name} ${item.manager_details.manager_last_name}`.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.project_name.toLowerCase().includes(searchText.toLowerCase()) ||
+        new Date(item.start_date).toLocaleDateString().includes(searchText) ||
+        new Date(item.end_date).toLocaleDateString().includes(searchText) ||
+        item.work_location.toLowerCase().includes(searchText.toLowerCase())
+      ) : true;
+  
+      return isStatusMatch && isSearchMatch;
+    });
+
+    const hiringStatuses = [...new Set(hiringList.map(item => item.hiring_status))];
+
+    const handleRowClick = (row) => {
+      setHiringModalDetails([]);
+      setHiringModalComments([]);
+
+      setIsOpen(true);
+        axios.get(`/hirings/${row.hiring_id}`)
           .then((response) => {
             setHiringModalDetails(response.data.hirings[0])
           })
           .catch((error) => {
             console.log(error);
           })
-          axios.get(`/hirings/hiringcomments/${hiringId}`)
+          axios.get(`/hirings/hiringcomments/${row.hiring_id}`)
           .then((response) => {
             setHiringModalComments(response.data.comments)
           })
           .catch((error) => {
             console.log(error);
           })
-        
-    }
+    };
 
     return (
       <Layout>
@@ -52,53 +85,38 @@ function EnquiredByMe() {
           <div className="card w-auto">
             <div className="card-header">
               <div className="row">
+                <div className="col input-group">
+                  <span className="input-group-text"><i className="bi bi-search text-gray"></i></span>
+                    <select style={{width:"35%"}} name="searchKey" id="search-key" value={selectedStatus} onChange={(e) => setSelectedStatus(e.target.value)}> 
+                      <option value="">All Statuses</option>
+                      {hiringStatuses.map(status => (
+                        <option key={status} value={status}>{status}</option>
+                      ))}
+                    </select>
+                    <input style={{width:"35%"}} className="ms-1" id="search-value" type="text" placeholder="Type a value" value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+                </div>
                 <div className="col text-center">
                   <h4>Hiring Enquired To Me</h4>
+                </div>
+                <div className="col">
                 </div>
               </div>
             </div>
             <div className="card-body table-responsive">
-              <table className="table table-hover">
-                <thead className="bg-light">
-                  <tr>
-                    <th>Employee Name</th>
-                    <th>Manager Name</th>
-                    <th>Manager Email</th>
-                    <th>Project Name</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Work Location</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {hiringList.map((hiringList, key) => {
-                    return (
-                      <tr 
-                        key={key}
-                        onClick={(e) => openModal(hiringList.hiring_id)}
-                      >
-                        
-                        <td>
-                          <Link
-                            to={`/projectShow/${hiringList.emp_id}`}
-                          >
-                            {hiringList.employee_details.first_name} {hiringList.employee_details.last_name}
-                          </Link>
-                        </td>
-                        <td>{hiringList.manager_details.manager_first_name}, {hiringList.manager_details.manager_last_name}</td>
-
-                        <td>{hiringList.manager_details.manager_email}</td>
-                        <td>{hiringList.project_name}</td>
-                        <td>{Utils.formatDateYYYYMMDD(hiringList.start_date)}</td>
-                        <td>{Utils.formatDateYYYYMMDD(hiringList.end_date)}</td>
-                        <td>{hiringList.work_location}</td>
-                        <td>{hiringList.hiring_status}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+            <DataTable
+                columns={columns}
+                data={filteredData}
+                pagination
+                onRowClicked={handleRowClick}
+              />
+              <HiringModal
+                modelStatus={modalIsOpen}
+                hiringDetails={hiringModalDetails}
+                hiringComments={hiringModalComments}
+                hideAddInListBtn={true}
+                hideHireBtn={true}
+                navigateToPage="/enquiredbyme"
+              />
               <HiringModal
                 modelStatus={modalIsOpen}
                 hiringDetails={hiringModalDetails}
